8408c17fa5266142c06f186ece1f0f9d5f9b1122
nmasharani <nmasharani@gmail.com>
2020-08-29 13:28:09 -0700

    want to start again

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import logo from './hotdog-dog.png';
 import ReactDOM from 'react-dom';
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
-
-import CheckoutForm from './CheckoutForm';
 import './App.css';
 
 function App() {
@@ -15,18 +11,6 @@ function App() {
         <p>
           Hello, world!
         </p>
-
-        {/*
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      */}
-
       </header>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import CheckoutForm from './CheckoutForm';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
+function App() {
+  return (
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
