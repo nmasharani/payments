da8692b20c2f28057633fcffa6480a5391bdad4b
nmasharani <nmasharani@gmail.com>
2020-08-31 21:37:57 -0700

    it kind of works and passes test cases

--- client/src/CheckoutForm.js
+++ client/src/CheckoutForm.js
@@ -20,38 +20,38 @@ export default function CheckoutForm() {
 
     const fetch = require('node-fetch');
 
-    var res = null;
-    var json = null;
     console.log("Trying to fetch");
 
     fetch("http://localhost:4242/secret", {
       method: "get", mode: "cors"
     })
     .then(res => res.json())
-    .then(json => console.log(json));
-/*
-    const result = await stripe.confirmCardPayment(res.client_secret, {
-      payment_method: {
-        card: elements.getElement(CardElement),
-        billing_details: {
-          name: 'Jenny Rosen',
-        },
+    .then(async function(json){
+      const result = await stripe.confirmCardPayment(json.client_secret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name: 'Jenny Rosen',
+          },
+        }
+      });
+
+      if (result.error) {
+        // Show error to your customer (e.g., insufficient funds)
+        console.log(result.error.message);
+      } else {
+        // The payment has been processed!
+        console.log(result);
+        if (result.paymentIntent.status === 'succeeded') {
+          // Show a success message to your customer
+          // There's a risk of the customer closing the window before callback
+          // execution. Set up a webhook or plugin to listen for the
+          // payment_intent.succeeded event that handles any business critical
+          // post-payment actions.
+          console.log("woohoo");
+        }
       }
     });
-
-    if (result.error) {
-      // Show error to your customer (e.g., insufficient funds)
-      console.log(result.error.message);
-    } else {
-      // The payment has been processed!
-      if (result.paymentIntent.status === 'succeeded') {
-        // Show a success message to your customer
-        // There's a risk of the customer closing the window before callback
-        // execution. Set up a webhook or plugin to listen for the
-        // payment_intent.succeeded event that handles any business critical
-        // post-payment actions.
-      }
-    }*/
   };
 
   return (
